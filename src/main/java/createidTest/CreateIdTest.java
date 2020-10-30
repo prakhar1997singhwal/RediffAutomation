687dcf4a6d344e4cc26182805fc9ac16a166fdf2
Prakhar Singhwal <prakharsinghwal96@gmail.com>
2020-10-30 07:56:06 +0530

    made some changes in pom.xml

--- src/main/java/createidTest/CreateIdTest.java
+++ src/main/java/createidTest/CreateIdTest.java
@@ -7,7 +7,10 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import com.util.test.TestUtil;
 
